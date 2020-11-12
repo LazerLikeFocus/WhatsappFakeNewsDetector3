cc309800abe1a9c8361cdfbc29725203f897fe53
Edwardabk <abhibhagat866@gmail.com>
2020-11-12 08:46:27 +0530

    New

--- app/src/main/java/com/lazerlikefoucs/whatsappfakenewsdetector3/FakeorRealActivity2.java
+++ app/src/main/java/com/lazerlikefoucs/whatsappfakenewsdetector3/FakeorRealActivity2.java
@@ -2,8 +2,15 @@ package com.lazerlikefoucs.whatsappfakenewsdetector3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.content.res.loader.ResourcesLoader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -13,16 +20,35 @@ import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 
+import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurationException;
+import org.deeplearning4j.nn.modelimport.keras.preprocessing.text.KerasTokenizer;
+import org.nd4j.common.io.Resource;
+import org.tensorflow.lite.Interpreter;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class FakeorRealActivity2 extends AppCompatActivity {
 
     private TextView detecttext;
     private EditText newstext;
     private ImageButton detect;
-    private String stringToPython;
+    private String stringToPython, path;
     public String[] separated;
+    public Interpreter tflite;
+    //public Path path;
 
+    public static KerasTokenizer kerasTokenizer;
 
     public int maxlen = 42;
     //public int maxlen = 500;
@@ -41,11 +67,16 @@ public class FakeorRealActivity2 extends AppCompatActivity {
         detect = findViewById(R.id.imageButton_detect);
         detecttext = findViewById(R.id.textView_detect);
 
+        try {
+            tflite = new Interpreter(loadModelFile());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         if (!Python.isStarted()){
             Python.start(new AndroidPlatform(this));
         }
 
-        //pythonInitialize();
 
         detect.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,6 +86,13 @@ public class FakeorRealActivity2 extends AppCompatActivity {
 
                     pythonInitialize();
                     //clean_string(stringToPython);
+                    try {
+                        tokenizer();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (InvalidKerasConfigurationException e) {
+                        e.printStackTrace();
+                    }
 
                     //detecttext.setText(stringToPython);
 
@@ -65,17 +103,55 @@ public class FakeorRealActivity2 extends AppCompatActivity {
         });
     }
 
+    //tokenize + pad Seq + encode docs
+    private void tokenizer() throws IOException, InvalidKerasConfigurationException {
+
+        //AssetManager am = this.getApplicationContext().getAssets();
+        //InputStream is = am.open("tokenizer.json");
+        //kerasTokenizer.fromJson(String.valueOf(is));
+
+        //kerasTokenizer.fromJson("file:///android_asset/src\\main\\assets\\tokenizer.json");
+
+        //kerasTokenizer.fromJson("app/src/main/assets/tokenizer.json");
+
+        //kerasTokenizer.fromJson("file:///android_asset/tokenizer.json");
+
+        //kerasTokenizer.fromJson("src/main/python/tokenizer.json");
+
+        //String path = this.getApplicationContext().getFilesDir().getAbsolutePath();
+        //File file = new File(path + "\\src\\main\\assets\\tokenizer.json");
+
+        //kerasTokenizer.fromJson(file.getPath());
+        System.out.println(path);
+
+        kerasTokenizer = KerasTokenizer.fromJson(path);
+
+        String line = stringToPython;
+        //String regex = "[^\\d]+";
+        String regex = "\\W";
+
+        String[] string = line.split(regex);
+
+        Integer[][] docs = kerasTokenizer.textsToSequences(string);
+
+        Integer[][] arr2 = Arrays.copyOf(docs, maxlen);
+
+        for (int i=0; i < maxlen; i++){
+            System.out.println(arr2[0][i]);
+            //data[i] = arr2[0][i];
+        }
+    }
+
 
     //python
     private void pythonInitialize() {
 
         Python py = Python.getInstance();
         final PyObject pyObj  = py.getModule("clean_string");
-        PyObject obj = pyObj.callAttr("find",stringToPython);
-        stringToPython = obj.toString();
+        PyObject obj = pyObj.callAttr("token");
+        path = obj.toString();
 
-        //data = pyObj.callAttr("find", stringToPython).toJava(Array[].class);
-
+/*        //data = pyObj.callAttr("find", stringToPython).toJava(Array[].class);
 
         //String str = stringToPython.replaceAll("[^0-9]", " ");
 
@@ -104,9 +180,48 @@ public class FakeorRealActivity2 extends AppCompatActivity {
 
             //data[i] = Float.parseFloat(string[i+1]);
             data[i] = Integer.parseInt(string[i+1]);
-        }
+        }*/
     }
 
+
+    //tflite
+    private float doInference(String inputString) {
+
+        //change input and output dims
+        //input is 42d array
+        //output is 1d array
+
+        //input shape from spyder is (None, 42)
+        float[][] inputVal = new float[1][maxlen];
+        //int[][] inputVal = new int[1][maxlen];
+
+        for (int i=0; i < maxlen; i++){
+            inputVal[0][i] = data[i]; // here we need to covert the stringtopython string to an array.
+            System.out.println("itemTF = " + inputVal[0][i]);
+        }
+
+        //output shape from spyder is (None, 1)
+        float[][] outputVal = new float[1][1]; //here we need to output a single digit.
+
+        //predict on model
+        tflite.run(inputVal, outputVal);
+
+        //inferred value is at [0]
+        float inferredVal = outputVal[0][0];
+
+        return inferredVal;
+    }
+
+    private MappedByteBuffer loadModelFile() throws IOException {
+
+        AssetFileDescriptor fileDescriptor = this.getAssets().openFd("model_whatsapp_fakenews.tflite");
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
 
 
     //data cleaning
@@ -123,6 +238,23 @@ public class FakeorRealActivity2 extends AppCompatActivity {
         }
     }
 
+    /*
+    //tokenizer
+    private void tokenizer() {
 
+        String sentence = stringToPython;
+
+        //Instantiating SimpleTokenizer class
+        SimpleTokenizer simpleTokenizer = SimpleTokenizer.INSTANCE;
+
+        //Tokenizing the given sentence
+        String tokens[] = simpleTokenizer.tokenize(sentence);
+
+        //Printing the tokens
+        for(String token : tokens) {
+            System.out.println(token);
+        }
+    }
+    */
 
 }
--- app/src/main/python/clean_string.py
+++ app/src/main/python/clean_string.py
@@ -5,47 +5,11 @@ Created on Fri Oct 30 12:31:15 2020
 @author: Abhishek
 """
 
-import numpy
-import pickle
-#import nltk
-import string
-#from nltk.corpus import stopwords
-#from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 from os.path import dirname, join
-filename_pos = join(dirname(__file__), "tokenizer.pickle")
+filename_pos = join(dirname(__file__), "tokenizer.json")
 
-maxlength = 42
-#maxlength = 500
-
-def find(X):
-    X =  X.split()
-    table = str.maketrans('', '', string.punctuation)
-    X = [w.translate(table) for w in X]
-    X = [word for word in X if word.isalpha()]
-    #stop_words = set(stopwords.words('english'))
-    #X = [w for w in X if not w in stop_words]
-    X = [word for word in X if len(word) > 1]
-        
-    #tokenizer = Tokenizer()
-
-    # loading
-    with open(filename_pos, 'rb') as handle:
-        tokenizer = pickle.load(handle)
-
-    #tokenizer.fit_on_texts(X)
-    
-    encoded_docs = tokenizer.texts_to_sequences(X)
-    
-    Xtest = pad_sequences(encoded_docs, maxlen=maxlength, padding='post')
-
-    Xtest = Xtest[:,0]
-
-    Xtest = numpy.pad(Xtest, (0, maxlength - Xtest.shape[0]), 'constant')
-
-    Xtest = Xtest.reshape(1, Xtest.shape[0])
-    
-    return Xtest
+def token():
+    return filename_pos
 
 # s = 'Provided to YouTube by Saregama India Ltd Mann Ki Lagan · Rahat Fateh Ali Khan Ek Cup Love℗ 2020 Saregama India LtdReleased on: 1969-09-27'
 # print(find(s))
